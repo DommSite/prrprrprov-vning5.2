e3456468146abf9328912c4d43aae08cf5521573
DommSite <johan.johansson06@live.se>
2023-11-22 10:12:06 +0100

    Add project files.

---
+++ prrprrprovövning5.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace prrprrprovCA52
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Hur många tal vill du skriva in?");
+        int[] tal = new int[int.Parse(Console.ReadLine())];
+        for(int i = 0; i < tal.Length; i++)
+        {
+            Console.WriteLine("Skriv in " + (i+1) + " heltal");
+            tal[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Här är talen du skrev in");
+        for (int i = 0;i < tal.Length; i++)
+        {
+            Console.WriteLine("Tal " + (i+1) + ": " + tal[i]);
+        }
+        }
+    }
+}
